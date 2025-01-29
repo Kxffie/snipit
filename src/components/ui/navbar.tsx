9fa6cdbe168c4369edc4b0e9392dcdcd998f5144
Kxffie <bryceriordan2005@gmail.com>
2025-01-29 08:33:23 -0600

    push

---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+      <nav className="w-full p-4 bg-background shadow-md">
+        <h1 className="text-xl font-bold">SnipIt</h1>
+      </nav>
+    );
+  }
+  
